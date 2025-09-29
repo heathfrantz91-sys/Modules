b2f2dde96a6fbcd4d67463e9616705aa0676ca63
heathfrantz91-sys <heathfrantz91@gmail.com>
2025-09-29 12:44:12 -0400

    Update main.tf

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,38 +1,34 @@
-provider "aws" {
-  region = "us-east-1" # Optional: can be overridden by CLI/env
+variable "name" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "ami" {}
+variable "subnet_id" {}
+variable "vpc_security_group_ids" {}
+variable "tags" {
+  type = map(string)
+}
+variable "cpu_core_count" {
+  type    = number
+  default = null
+}
+variable "cpu_threads_per_core" {
+  type    = number
+  default = null
+}
+variable "create" {
+  type    = bool
+  default = true
 }
 
-# Automatically select latest Ubuntu AMI if none provided
-data "aws_ami" "default_ubuntu" {
-  count       = var.ami == "" ? 1 : 0
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical (Ubuntu)
-}
-
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 5.6.0"
-
+resource "aws_instance" "this" {
   count = var.create ? 1 : 0
 
-  name          = var.name
+  ami           = var.ami
   instance_type = var.instance_type
-  ami           = var.ami != "" ? var.ami : data.aws_ami.default_ubuntu[0].id
   key_name      = var.key_name
-
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.security_group_ids
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags = var.tags
 
   # ✅ Only set CPU options if both values are provided
   dynamic "cpu_options" {
@@ -42,11 +38,4 @@ module "ec2_instance" {
       threads_per_core = var.cpu_threads_per_core
     }
   }
-
-  tags = merge(
-    {
-      "Name" = var.name
-    },
-    var.instance_tags
-  )
 }
