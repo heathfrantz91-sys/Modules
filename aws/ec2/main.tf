629a28a8dda7b38bd2caf716d558939424767982
heathfrantz91-sys <heathfrantz91@gmail.com>
2025-09-29 12:50:00 -0400

    Update main.tf

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,36 +1,13 @@
-variable "name" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "ami" {}
-variable "subnet_id" {}
-variable "vpc_security_group_ids" {}
-variable "tags" {
-  type = map(string)
-}
-variable "cpu_core_count" {
-  type    = number
-  default = null
-}
-variable "cpu_threads_per_core" {
-  type    = number
-  default = null
-}
-variable "create" {
-  type    = bool
-  default = true
-}
-
 resource "aws_instance" "this" {
   count = var.create ? 1 : 0
 
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
-  tags = var.tags
+  tags                   = var.tags
 
-  # ✅ Only set CPU options if both values are provided
   dynamic "cpu_options" {
     for_each = var.cpu_core_count != null && var.cpu_threads_per_core != null ? [1] : []
     content {
