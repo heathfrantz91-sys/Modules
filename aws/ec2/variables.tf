e8021fe06ce414bcc60d5096e1328e9b3ba8f3fd
heathfrantz91-sys <heathfrantz91@gmail.com>
2025-09-29 12:44:29 -0400

    Update variables.tf

--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -1,64 +1,53 @@
-variable "create" {
-  description = "Whether to create an EC2 instance"
-  type        = bool
-  default     = true
-}
-
 variable "name" {
-  description = "Name of the EC2 instance"
+  description = "EC2 instance name"
   type        = string
-  default     = "unknownec2instance"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
-}
-
-variable "ami" {
-  description = "Optional AMI ID. If empty, the latest Ubuntu 20.04 will be used automatically."
-  type        = string
-  default     = ""
 }
 
 variable "key_name" {
-  description = "SSH key pair name to attach to the EC2 instance"
+  description = "SSH key name"
   type        = string
-  default     = null
+}
+
+variable "ami" {
+  description = "AMI ID for EC2"
+  type        = string
 }
 
 variable "subnet_id" {
-  description = "Subnet ID where the instance will be launched"
+  description = "Subnet ID to launch EC2"
   type        = string
 }
 
-variable "security_group_ids" {
-  description = "List of security group IDs to associate with the instance"
+variable "vpc_security_group_ids" {
+  description = "List of security group IDs"
   type        = list(string)
-  default     = []
 }
 
-variable "instance_tags" {
-  description = "Additional tags to apply to the EC2 instance"
+variable "tags" {
+  description = "Tags to apply to instance"
   type        = map(string)
   default     = {}
 }
 
-variable "environment" {
-  description = "Environment (e.g. dev, qa, prod)"
-  type        = string
-  default     = "dev"
-}
-
 variable "cpu_core_count" {
-  description = "Number of CPU cores (only for compatible instance types; set null to skip)"
+  description = "Number of CPU cores, optional"
   type        = number
   default     = null
 }
 
 variable "cpu_threads_per_core" {
-  description = "Number of threads per core (only for compatible instance types; set null to skip)"
+  description = "Threads per CPU core, optional"
   type        = number
   default     = null
+}
+
+variable "create" {
+  description = "Whether to create the EC2 instance"
+  type        = bool
+  default     = true
 }
