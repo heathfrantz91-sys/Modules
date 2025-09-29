7dbf327546407a504a13bd918badeeaeb2c5def4
heathfrantz91-sys <heathfrantz91@gmail.com>
2025-09-29 11:47:36 -0400

    Update variables.tf

--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -17,48 +17,48 @@ variable "instance_type" {
 }
 
 variable "ami" {
-  description = "Optional AMI ID (if empty, latest Ubuntu will be used)"
+  description = "Optional AMI ID. If empty, the latest Ubuntu 20.04 will be used automatically."
   type        = string
   default     = ""
 }
 
 variable "key_name" {
-  description = "SSH key pair to attach"
+  description = "SSH key pair name to attach to the EC2 instance"
   type        = string
   default     = null
 }
 
 variable "subnet_id" {
-  description = "Subnet ID where instance will be launched"
+  description = "Subnet ID where the instance will be launched"
   type        = string
 }
 
 variable "security_group_ids" {
-  description = "List of security group IDs"
+  description = "List of security group IDs to associate with the instance"
   type        = list(string)
   default     = []
 }
 
 variable "instance_tags" {
-  description = "Additional tags for the instance"
+  description = "Additional tags to apply to the EC2 instance"
   type        = map(string)
   default     = {}
 }
 
 variable "environment" {
-  description = "Environment (dev, qa, prod)"
+  description = "Environment (e.g. dev, qa, prod)"
   type        = string
   default     = "dev"
 }
 
 variable "cpu_core_count" {
-  description = "Number of CPU cores for the instance (only supported on compatible instance types). Leave null to skip."
+  description = "Number of CPU cores (only for compatible instance types; set null to skip)"
   type        = number
   default     = null
 }
 
 variable "cpu_threads_per_core" {
-  description = "Threads per core for the instance (only supported on compatible instance types). Leave null to skip."
+  description = "Number of threads per core (only for compatible instance types; set null to skip)"
   type        = number
   default     = null
 }
