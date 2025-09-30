174c321fdfdb1e90fef871a45c27ebf53395d3b4
Heath Frantz <heathfrantz91@gmail.com>
2025-09-30 12:33:54 -0400

    work idiot

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,19 +1,25 @@
-resource "aws_instance" "this" {
-  count = var.create ? 1 : 0
-
+module "ec2-instance" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "6.1.1"
   ami                    = var.ami
-  instance_type          = var.instance_type
   key_name               = var.key_name
+  instance_type          = var.instance_type
   subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids != null ? var.vpc_security_group_ids : var.security_group_ids
-  tags                   = var.tags
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags                   = { Name = var.name }
+}
 
-  # Dynamic CPU options inside the module
-  dynamic "cpu_options" {
-    for_each = var.cpu_core_count != null && var.cpu_threads_per_core != null ? [1] : []
-    content {
-      core_count       = var.cpu_core_count
-      threads_per_core = var.cpu_threads_per_core
-    }
-  }
+github repo
+provider "aws" {
+  region = "us-east-1"
+}
+module "ec2_instance" {
+  source = "git::https://github.com/heathfrantz91-sys/Modules.git//aws/ec2?ref=main"
+
+  ami                    =  var.ami
+  key_name               = var.key_name
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  tags                   = { Name = var.name }  
 }
