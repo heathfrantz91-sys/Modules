07a43f1dd9ae005cf101ea4d386566942f349a6e
Heath Frantz <heathfrantz91@gmail.com>
2025-09-30 13:40:23 -0400

    piss

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,25 +1,14 @@
-module "ec2-instance" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "6.1.1"
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source = "git::https://github.com/heathfrantz91-sys/Git-hub-IAC-pipelines.git//infrastructure/ec2?ref=main"
+
   ami                    = var.ami
   key_name               = var.key_name
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   tags                   = { Name = var.name }
-}
-
-github repo
-provider "aws" {
-  region = "us-east-1"
-}
-module "ec2_instance" {
-  source = "git::https://github.com/heathfrantz91-sys/Git-hub-IAC-pipelines.git//infrastructure/ec2?ref=main"
-
-  ami                    =  var.ami
-  key_name               = var.key_name
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
-  tags                   = { Name = var.name }  
 }
