d1147b75a00ae3bee0b47e220ec7bc96ea2d5fc1
Heath Frantz <heathfrantz91@gmail.com>
2025-10-01 12:02:16 -0400

    filthy

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -5,7 +5,3 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = var.vpc_security_group_ids
   key_name               = var.key_name
   tags                   = var.tags
-
-  cpu_core_count         = var.cpu_core_count
-  cpu_threads_per_core   = var.cpu_threads_per_core
-}
