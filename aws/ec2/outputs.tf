c4958821e0b880f8b6298b2bfd256ed193e9ec95
Heath Frantz <heathfrantz91@gmail.com>
2025-10-01 12:00:38 -0400

    butthole

--- aws/ec2/outputs.tf
+++ aws/ec2/outputs.tf
@@ -1,11 +1,14 @@
-output "instance_id" {
-  value = aws_instance.this[*].id
+output "id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.this.id
 }
 
 output "public_ip" {
-  value = aws_instance.this[*].public_ip
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.this.public_ip
 }
 
 output "private_ip" {
-  value = aws_instance.this[*].private_ip
+  description = "The private IP address of the EC2 instance"
+  value       = aws_instance.this.private_ip
 }
