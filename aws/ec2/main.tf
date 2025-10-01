637842e5e101c098d5e95653c094591649c37c16
Heath Frantz <heathfrantz91@gmail.com>
2025-10-01 11:47:36 -0400

    yolo swaggins

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,14 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "ec2_instance" {
-  source = "git::https://github.com/heathfrantz91-sys/Git-hub-IAC-pipelines.git//infrastructure/ec2?ref=main"
-
+resource "aws_instance" "this" {
   ami                    = var.ami
-  key_name               = var.key_name
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
-  tags                   = { Name = var.name }
+  key_name               = var.key_name
+  tags                   = var.tags
 }
